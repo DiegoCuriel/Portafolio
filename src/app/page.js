e4d01bd3702777e3f0ebc14617f2715b49ee3ff3
DiegoCuriel <a01640372@itesm.mx>
2023-12-23 13:24:24 -0600

    Navbar - image & li

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,8 @@
 import Navbar from "../components/Navbar";
-import Head from "next/head";
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Portafolio</title>
-        <meta name="description" content="Portafolio de proyectos" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
       <Navbar />
     </div>
